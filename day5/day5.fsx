315cd74c8b4609666af9bf08489027f802bfccfb
Lari Tuomisto <lari.tuomisto@solita.fi>
2021-12-09 23:28:34 +0200

    day 5 formatting

--- day5/day5.fsx
+++ day5/day5.fsx
@@ -1,31 +1,19 @@
-let log tx x =
-    x |> printfn "%s %A" tx
-    x
-
 let isHV x1 x2 y1 y2 = x1 = x2 || y1 = y2
+
+let isInHVLine (x1, y1, x2, y2) (x, y) =
+    x >= min x1 x2
+    && x <= max x1 x2
+    && y >= min y1 y2
+    && y <= max y1 y2
 
 let isWithin (x1, y1, x2, y2) (x, y) =
     match (isHV x1 x2 y1 y2) with
-    | true ->
-        if (x >= min x1 x2
-            && x <= max x1 x2
-            && y >= min y1 y2
-            && y <= max y1 y2) then
-            1
-        else
-            0
+    | true when isInHVLine (x1, y1, x2, y2) (x, y) -> 1
     | false ->
-        let dx =
-            ((x - x1) |> double) / ((x2 - x1) |> double)
-
-        let dy =
-            ((y - y1) |> double) / ((y2 - y1) |> double)
-
-        if dx = dy && dx >= 0. && dx <= 1. then
-            1
-        else
-            0
-
+        let dx = ((x - x1) |> double) / ((x2 - x1) |> double)
+        let dy = ((y - y1) |> double) / ((y2 - y1) |> double)
+        if dx = dy && dx >= 0. && dx <= 1. then 1 else 0
+    | _ -> 0
 
 let input =
     System.IO.File.ReadAllLines "./day5/day5.txt"
@@ -40,6 +28,5 @@ let coords =
     ||> Array.allPairs
     |> Array.map (fun coord -> Seq.sumBy (fun fn -> fn coord) input)
     |> Array.countBy (fun f -> f > 1)
-    |> printfn "%A"
 
-coords
+coords |> printfn "%A"
