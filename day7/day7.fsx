8b038542613df98b05c6d872e4c576d8c7e621d0
Lari Tuomisto <lari.tuomisto@solita.fi>
2021-12-17 01:46:41 +0200

    day 7

--- day7/day7.fsx
+++ day7/day7.fsx
@@ -1,19 +1,24 @@
 let fish 
-    = "./day7/day7test.txt" 
+    = "./day7/day7.txt" 
     |> System.IO.File.ReadAllText 
     |> fun s -> s.Split ',' 
     |> Array.map int
 
 let gt = Seq.max fish
 
-let range grt i
-    =  [i .. -1 .. 0] @ [1 .. grt - i]
+let a2 = [0 .. gt] |> Seq.map (fun a -> Seq.sum [0 .. a])
+let a1 = seq [0 .. gt]
 
-//Answer 1
-fish 
-    |> Seq.map (range gt) 
-    |> Seq.transpose
-    |> Seq.minBy Seq.sum
-    |> Seq.sum
-    |> printfn "Answer 1: %A"
-    
+let range grt costs i = 
+    [costs |> Seq.take i |> Seq.sortDescending ] @ [costs |> Seq.tail |> Seq.take (grt - i)] |> Seq.concat
+
+let getanswer a =
+    fish 
+        |> Seq.map (range gt a) 
+        |> Seq.transpose
+        |> Seq.minBy Seq.sum
+        |> Seq.sum
+        |> printfn "Answer: %A"
+
+getanswer a1
+getanswer a2
