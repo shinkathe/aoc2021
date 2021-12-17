de7b56accefdbbf534fa6460c208c7ef8691cd25
Lari Tuomisto <lari.tuomisto@solita.fi>
2021-12-17 02:05:20 +0200

    day 7 fixes

--- day7/day7.fsx
+++ day7/day7.fsx
@@ -1,24 +1,14 @@
-let fish 
-    = "./day7/day7.txt" 
-    |> System.IO.File.ReadAllText 
-    |> fun s -> s.Split ',' 
-    |> Array.map int
+let input = "./day7/day7.txt" |> System.IO.File.ReadAllText |> fun s -> s.Split ',' |> Array.map int
 
-let gt = Seq.max fish
+let a1 gt = seq [0 .. gt]
+let a2 gt = [0 .. gt] |> Seq.map (fun a -> Seq.sum [0 .. a])
 
-let a2 = [0 .. gt] |> Seq.map (fun a -> Seq.sum [0 .. a])
-let a1 = seq [0 .. gt]
+let range costs i = 
+    [costs |> Seq.take i |> Seq.sortDescending ; costs |> Seq.tail |> Seq.take ((Seq.max input) - i)] |> Seq.concat
 
-let range grt costs i = 
-    [costs |> Seq.take i |> Seq.sortDescending ] @ [costs |> Seq.tail |> Seq.take (grt - i)] |> Seq.concat
+let getanswer a input =
+    let gt = a (Seq.max input)
+    input |> Seq.map (range gt) |> Seq.transpose |> Seq.minBy Seq.sum |> Seq.sum
 
-let getanswer a =
-    fish 
-        |> Seq.map (range gt a) 
-        |> Seq.transpose
-        |> Seq.minBy Seq.sum
-        |> Seq.sum
-        |> printfn "Answer: %A"
-
-getanswer a1
-getanswer a2
+getanswer a1 input |> printfn "Answer: %A"
+getanswer a2 input |> printfn "Answer: %A"
