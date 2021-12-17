9a5a08836aa1b3c8556fc0962f198e41a5a7cafc
Lari Tuomisto <lari.tuomisto@solita.fi>
2021-12-17 21:41:14 +0200

    optimize day 7 perf

--- day7/day7.fsx
+++ day7/day7.fsx
@@ -1,14 +1,16 @@
+#time
+
 let input = "./day7/day7.txt" |> System.IO.File.ReadAllText |> fun s -> s.Split ',' |> Array.map int
 
-let a1 gt = seq [0 .. gt]
-let a2 gt = [0 .. gt] |> Seq.map (fun a -> Seq.sum [0 .. a])
+// let a1 gt = [0 .. gt] |> Array.toArray
+let a2 gt = [|0 .. gt|] |> Array.map (fun a -> Array.sum [|0 .. a|])
 
 let range costs i = 
-    [costs |> Seq.take i |> Seq.sortDescending ; costs |> Seq.tail |> Seq.take ((Seq.max input) - i)] |> Seq.concat
+    [costs |> Array.take i |> Array.sortDescending ; costs |> Array.tail |> Array.take ((Array.max input) - i)] |> Array.concat
 
 let getanswer a input =
-    let gt = a (Seq.max input)
-    input |> Seq.map (range gt) |> Seq.transpose |> Seq.minBy Seq.sum |> Seq.sum
+    let gt = a (Array.max input)
+    input |> Array.map (range gt) |> Array.transpose |> Array.minBy Array.sum |> Array.sum
 
-getanswer a1 input |> printfn "Answer: %A"
+// getanswer a1 input |> printfn "Answer: %A"
 getanswer a2 input |> printfn "Answer: %A"
