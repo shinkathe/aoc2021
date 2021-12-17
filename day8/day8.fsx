99884cbf137380c23f16255d11e0ab2cf34fd9c2
Lari Tuomisto <lari.tuomisto@solita.fi>
2021-12-17 10:38:35 +0200

    day8

--- day8/day8.fsx
+++ day8/day8.fsx
@@ -1,12 +1,11 @@
-// Answer 1
 let input = 
     System.IO.File.ReadAllLines "./day8/day8.txt" |> Seq.map ((fun (s:string) -> s.Split " | ") >> Seq.map (fun (x:string) -> x.Split " "));
 
 // Answer 1
 input 
-|> Seq.map (Seq.last)  
-|> Seq.concat
-|> Seq.where (fun x -> x.Length < 5 || x.Length = 7)
-|> Seq.length
-|> printfn "%A"
+    |> Seq.map (Seq.last)  
+    |> Seq.concat
+    |> Seq.where (fun x -> x.Length < 5 || x.Length = 7)
+    |> Seq.length
+    |> printfn "%A"
 
